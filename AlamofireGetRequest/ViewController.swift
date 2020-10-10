955dcf293c2c4d1eaa47b87121afc1d9b5c112e9
Maksat <minaidarr@gmail.com>
2020-10-10 13:24:59 +0500

    Initial Commit

---
+++ AlamofireGetRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireGetRequest
+//
+//  Created by Минайдар  Максат on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
